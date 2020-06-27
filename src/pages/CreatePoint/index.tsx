c8e6ee6d6d514da683da5d2782080f8cf1d943df
diegodls <dark.dls.lol@gmail.com>
2020-06-27 12:55:26 -0300

    Adicionado Dropzone para upload de imagens

--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -8,6 +8,8 @@ import axios from 'axios';
 import './styles.css';
 import logo from '../../assets/logo.svg';
 import api from '../../services/api';
+import Dropzone from '../../components/Dropzone'
+
 
 interface Item {
     id: number;
@@ -39,6 +41,7 @@ const CreatePoint = () => {
         whatsapp: ''
     });
     const [selectedItems, setSelectedItems] = useState<number[]>([]);
+    const [selectedFile, setSelectedFile] = useState<File>();
 
     const history = useHistory();
 
@@ -124,16 +127,21 @@ const CreatePoint = () => {
         const [latitude, longitude] = selectedPosition;
         const items = selectedItems;
 
-        const data = {
-            name,
-            email,
-            whatsapp,
-            uf,
-            city,
-            latitude,
-            longitude,
-            items
-        };
+        const data = new FormData();
+
+
+        data.append('name', name);
+        data.append('email', email);
+        data.append('whatsapp', whatsapp);
+        data.append('uf', uf);
+        data.append('city', city);
+        data.append('latitude', String(latitude));
+        data.append('longitude',  String(longitude));
+        data.append('items', items.join(','));
+        
+        if(selectedFile){
+            data.append('image', selectedFile);            
+        }
 
         console.log(data)
 
@@ -142,6 +150,8 @@ const CreatePoint = () => {
         history.push('/');
 
     }
+
+
 
 
 
@@ -157,6 +167,9 @@ const CreatePoint = () => {
             </header>
             <form onSubmit={handlerSubmit}>
                 <h1>Cadastro do <br />ponto de coleta</h1>
+
+                <Dropzone onFileUploaded={setSelectedFile} />
+
                 <fieldset>
                     <legend>
                         <h2>Dados</h2>
